b5a98d9c06b2d26523e9495a2e15b4fae0c0e397
Kavin Ajay <kavinajay777@gmail.com>
2025-08-31 15:43:12 +0530

    git

---
+++ Script.js
@@ -0,0 +1,17 @@
+const rootdir = document.getElementById("rosee");
+const heading = React.createElement("h1", {}, "vankoooo");
+const root = ReactDOM.createRoot(rootdir);
+
+const newElement = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", { id: "child2" }, "vaa la punda ")
+  )
+);
+
+const rose = React.createElement("h1", {}, "rosewin is topper");
+root.render(rose);
+console.log(rose);
